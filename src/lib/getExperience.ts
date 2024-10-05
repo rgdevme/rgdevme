97376eb3971ea5dd36da0c8e5f832e7da4b7ce0b
Rafael González <reggon81@gmail.com>
2024-10-05 12:17:18 -0400

    fix(components): transforme data from notion to final output

--- src/lib/getExperience.ts
+++ src/lib/getExperience.ts
@@ -1,8 +1,26 @@
+import {
+  CheckboxPropertyItemObjectResponse,
+  DatabaseObjectResponse,
+  SelectPropertyItemObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints'
 import { notion } from '../config/notion'
+import {
+  getDate,
+  getLinks,
+  getNotionTitle,
+  getRelations,
+  getRichText,
+  getSelect,
+} from '../utils/notion'
+
+type PartialSelectResponse = NonNullable<
+  SelectPropertyItemObjectResponse['select']
+>
 
 export const getExperience = async () => {
-  const { results } = await notion.databases.query({
-    database_id: process.env.EXP!,
+  const response = await notion.databases.query({
+    database_id: process.env.DBID_EXPERIENCE!,
+    filter: { and: [{ property: 'inactive', checkbox: { equals: false } }] },
     sorts: [
       {
         property: 'dates',
@@ -15,5 +33,84 @@ export const getExperience = async () => {
     ],
   })
 
-  return results
+  const results = response.results as DatabaseObjectResponse[]
+
+  const experience_category: PartialSelectResponse[] = []
+  const experience_contract: PartialSelectResponse[] = []
+  const experience_country: PartialSelectResponse[] = []
+
+  const experience = results.map(x => {
+    const category_select = getSelect(x.properties.category)
+    const contract_select = getSelect(x.properties.contract)
+    const country_select = getSelect(x.properties.country)
+    if (
+      category_select &&
+      !experience_category.some(y => y.id === category_select.id)
+    ) {
+      experience_category.push(category_select)
+    }
+    if (
+      contract_select &&
+      !experience_contract.some(y => y.id === contract_select.id)
+    ) {
+      experience_contract.push(contract_select)
+    }
+    if (
+      country_select &&
+      !experience_country.some(y => y.id === country_select.id)
+    ) {
+      experience_country.push(country_select)
+    }
+
+    return {
+      id: x.id as string,
+      cover: x.cover as string | null,
+      icon: x.icon as string | null,
+      created: x.created_time as string,
+      updated: x.last_edited_time as string,
+      category: category_select?.id ?? null,
+      contract: contract_select?.id ?? null,
+      country: country_select?.id ?? null,
+      completed:
+        (
+          x.properties
+            .completed as unknown as CheckboxPropertyItemObjectResponse
+        ).checkbox ?? null,
+      start: getDate(x.properties.dates)!.start,
+      end: getDate(x.properties.dates)!.end,
+      project:
+        getRichText(x.properties.project_en)
+          .map(y => y.plain_text)
+          .join('') ?? null,
+      description:
+        getRichText(x.properties.desc_en)
+          .map(y => y.plain_text)
+          .join('') ?? null,
+      role:
+        getRichText(x.properties.role_en)
+          .map(y => y.plain_text)
+          .join('') ?? null,
+      client:
+        getNotionTitle(x.properties.client)
+          .map(y => y.plain_text)
+          .join('') ?? null,
+      institution:
+        getRichText(x.properties.institution)
+          .map(y => y.plain_text)
+          .join('') ?? null,
+      skills: getRelations(x.properties.skills).map(y => y.id ?? null) ?? null,
+      links: getLinks(x.properties.link),
+    }
+  })
+
+  return {
+    experience,
+    experience_category,
+    experience_contract,
+    experience_country,
+  }
 }
+
+export type ExperienceItem = Awaited<
+  ReturnType<typeof getExperience>
+>['experience'][number]
--- src/lib/getSkills.ts
+++ src/lib/getSkills.ts
@@ -1,11 +1,33 @@
+import {
+  DatabaseObjectResponse,
+  NumberPropertyItemObjectResponse,
+  SelectPropertyItemObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints'
 import { notion } from '../config/notion'
+import {
+  getLinks,
+  getMultiSelect,
+  getNotionTitle,
+  getNumber,
+  getRelations,
+  getRichText,
+  getSelect,
+} from '../utils/notion'
+
+type PartialSelectResponse = NonNullable<
+  SelectPropertyItemObjectResponse['select']
+>
 
 export const getSkills = async () => {
-  const { results } = await notion.databases.query({
-    database_id: process.env.SKL!,
+  const response = await notion.databases.query({
+    database_id: process.env.DBID_SKILLS!,
     sorts: [
       {
-        property: 'category',
+        property: 'area',
+        direction: 'ascending',
+      },
+      {
+        property: 'type',
         direction: 'ascending',
       },
       {
@@ -15,5 +37,51 @@ export const getSkills = async () => {
     ],
   })
 
-  return results
+  const results = response.results as DatabaseObjectResponse[]
+
+  const skill_tags: PartialSelectResponse[] = []
+  const skill_type: PartialSelectResponse[] = []
+  const skills = results.map(x => {
+    const area = getSelect(x.properties.area)?.id ?? null
+    const type_select = getSelect(x.properties.type)
+    const experience = getRelations(x.properties.experience).map(x => x.id)
+    const tags = getMultiSelect(x.properties.tags)
+    const level = getNumber(x.properties.level)
+
+    if (type_select && !skill_type.some(y => y.id === type_select.id)) {
+      skill_type.push(type_select)
+    }
+
+    tags.forEach(t => {
+      if (skill_tags.some(y => y.id === t.id)) return
+      skill_tags.push(t)
+    })
+
+    return {
+      id: x.id as string,
+      cover: x.cover as string | null,
+      icon: x.icon as string | null,
+      created: x.created_time as string,
+      updated: x.last_edited_time as string,
+      area,
+      type: type_select?.id ?? null,
+      level:
+        type_select?.name === 'Language' && level !== null
+          ? ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'Native'][level]
+          : null,
+      institution:
+        getRichText(x.properties.institution)
+          .map(x => x.plain_text)
+          .join('') ?? null,
+      name:
+        getNotionTitle(x.properties.name)
+          .map(x => x.plain_text)
+          .join('') ?? null,
+      experience,
+      use_count: experience.length,
+      certifications: getLinks(x.properties.certifications),
+    }
+  })
+
+  return { skills, skill_tags, skill_type }
 }
