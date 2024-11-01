69d3103ef6032d94ae57db9130d81cb655b1f8b5
Rafael González <reggon81@gmail.com>
2024-11-02 00:30:08 +0100

    feat(components): add metatags and context providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import type { AppProps } from 'next/app'
 import '../globals.css'
 
+import { createTheme, MantineProvider } from '@mantine/core'
+import Head from 'next/head'
+
+const theme = createTheme({})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+			</Head>
+			<MantineProvider theme={theme}>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</>
+	)
 }
