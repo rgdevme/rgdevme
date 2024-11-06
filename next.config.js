6b2c348cfc3347203b191ea8e219b52badd46faa
Rafael González <reggon81@gmail.com>
2024-11-06 18:01:22 +0100

    fix(repo): remove support for i18n while i get a domain

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-	i18n: {
-		// These are all the locales you want to support in
-		// your application
-		locales: ['en', 'fr', 'es', 'hu'],
-		// This is the default locale you want to be used when visiting
-		// a non-locale prefixed path e.g. `/hello`
-		defaultLocale: 'en'
-	},
+	// i18n: {
+	// 	// These are all the locales you want to support in
+	// 	// your application
+	// 	locales: ['en', 'fr', 'es', 'hu'],
+	// 	// This is the default locale you want to be used when visiting
+	// 	// a non-locale prefixed path e.g. `/hello`
+	// 	defaultLocale: 'en'
+	// },
 	images: {
 		unoptimized: true,
 		remotePatterns: [
