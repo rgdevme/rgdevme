1814270b1ae1fe13053dab078d9949b2f9a715dc
Rafael González <reggon81@gmail.com>
2024-11-06 13:36:58 +0100

    feat(repo): add support for i18n

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	i18n: {
-		locales: ['en'],
+		// These are all the locales you want to support in
+		// your application
+		locales: ['en', 'fr', 'es', 'hu'],
+		// This is the default locale you want to be used when visiting
+		// a non-locale prefixed path e.g. `/hello`
 		defaultLocale: 'en'
 	},
 	images: {
