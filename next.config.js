25974ef08f9db2ce2fed5e898eba45e40f35217f
Rafael González <reggon81@gmail.com>
2024-11-06 14:25:20 +0100

    fix(repo): try to fix deployment on github

--- next.config.js
+++ next.config.js
@@ -1,22 +1,25 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = () => ({ output: 'export' })
-// ({
-// 	i18n: {
-// 		// These are all the locales you want to support in
-// 		// your application
-// 		locales: ['en', 'fr', 'es', 'hu'],
-// 		// This is the default locale you want to be used when visiting
-// 		// a non-locale prefixed path e.g. `/hello`
-// 		defaultLocale: 'en'
-// 	},
-// 	images: {
-// 		unoptimized: true,
-// 		remotePatterns: [
-// 			{
-// 				protocol: 'https',
-// 				hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com'
-// 			}
-// 		]
-// 	}
-// })
+module.exports = () => {
+	return { output: 'export' }
+}
+/** ({
+ * i18n: {
+ * 	// These are all the locales you want to support in
+ * 	// your application
+ * 	locales: ['en', 'fr', 'es', 'hu'],
+ * 	// This is the default locale you want to be used when visiting
+ * 	// a non-locale prefixed path e.g. `/hello`
+ * 	defaultLocale: 'en'
+ * },
+ * images: {
+ * 	unoptimized: true,
+ * 	remotePatterns: [
+ * 		{
+ * 			protocol: 'https',
+ * 			hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com'
+ * 		}
+ * 	]
+ * }
+ * })
+ **/
