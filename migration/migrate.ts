39032dc6f98e2b8c8048ab811f564fd493cb8e87
Rafael González <reggon81@gmail.com>
2024-11-26 21:59:21 +0100

    fix(repo): transform data before migration

--- migration/migrate.ts
+++ migration/migrate.ts
@@ -1,14 +1,65 @@
-import { FirebormDataManager } from '../src/utils/importer'
-import { firestore } from '../src/firebase/config'
+import { fireborm } from '../src/firebase/config'
 import experience from './experience.json'
-import terms from './terms.json'
 import profile from './profile.json'
+import terms from './terms.json'
+
+const transformTerms = (data: typeof terms) =>
+	data.map(x => ({
+		...x,
+		id: x.id,
+		experience: x.experience
+			.toString()
+			.split(',')
+			.map(s => s.trim()),
+		level:
+			typeof x.level === 'string'
+				? x.level.length === 0
+					? 0
+					: Number(x.level)
+				: x.level,
+		tags: x.tags
+			.toString()
+			.split(',')
+			.map(s => s.trim())
+	}))
+
+const transformExperience = (data: typeof experience) =>
+	data.map(x => ({
+		...x,
+		id: x.id,
+		skills: x.skills
+			.toString()
+			.split(',')
+			.map(s => s.trim()),
+		start: x.start.length > 0 ? new Date(x.start) : null,
+		end: x.end.length > 0 ? new Date(x.end) : null
+	}))
 
 const importer = async () => {
-	const importer = new FirebormDataManager(firestore)
+	const importer = fireborm.initializeDataManager()
 
 	importer.import({
-		files: [{ collection: 'profile', objects: profile }]
+		files: {
+			profile,
+			experience: transformExperience(experience),
+			terms: transformTerms(terms)
+		},
+		ignore: { experience: ['id'], profile: [], terms: ['id'] },
+		relations: {
+			experience: [
+				{
+					from: { property: 'skills' },
+					to: { collection: 'terms', property: 'id' }
+				}
+			],
+			terms: [
+				{
+					from: { property: 'experience' },
+					to: { collection: 'experience', property: 'id' }
+				}
+			],
+			profile: []
+		}
 	})
 }
 
